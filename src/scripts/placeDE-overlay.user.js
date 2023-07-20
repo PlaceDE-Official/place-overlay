39690134980b33e1ac66d73a08e56ef874f7986a
Play_it <38607319+Playit3110@users.noreply.github.com>
2023-07-20 23:24:04 +0200

    Update placeDE-overlay.user.js to use fewer lines and make it more readable

--- src/scripts/placeDE-overlay.user.js
+++ src/scripts/placeDE-overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/placeDE Template
 // @namespace    http://tampermonkey.net/
-// @version      6
+// @version      5
 // @description  try to take over the canvas!
 // @author       placeDE Devs
 // @match        https://garlic-bread.reddit.com/embed*
@@ -10,19 +10,17 @@
 // @downloadURL  https://github.com/PlaceDE-Official/place-overlay/raw/main/src/scripts/placeDE-overlay.user.js
 // ==/UserScript==
 
-var overlayImage = null;
-if (window.top !== window.self) {
-    window.addEventListener('load', () => {
-        const canvasContainer = document.querySelector("garlic-bread-embed").shadowRoot.querySelector("div.layout").querySelector("garlic-bread-canvas").shadowRoot.querySelector("div.container");
+let overlayImage = null;
+if(window.top !== window.self) {
+    window.addEventListener("load", function() {
+        const canvasContainer = document.querySelector("garlic-bread-embed").shadowRoot.querySelector("garlic-bread-canvas").shadowRoot.querySelector(".container");
         overlayImage = document.createElement("img");
+        function updateImage() {
+            overlayImage.src = "https://place.army/overlay_target.png?" + Date.now()
+        }
         updateImage();
-        overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;`;
+        setInterval(updateImage, 30000);
+        overlayImage.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
         canvasContainer.appendChild(overlayImage);
     }, false);
 }
-
-function updateImage() {
-    overlayImage.src = "https://place.army/overlay_target.png?" + Date.now()
-}
-
-setInterval(function () {overlayImage.src = "https://place.army/overlay_target.png?" + Date.now()}, 30000);
