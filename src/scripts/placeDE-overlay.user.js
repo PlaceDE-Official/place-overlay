9aab8a9f382ca817d2d8163b20b35b98e3f9a0ff
Xanax-C-137 <moritz.fuessel@flawcra.cc>
2023-07-20 21:26:18 +0200

    Added auto-refresh

--- src/scripts/placeDE-overlay.user.js
+++ src/scripts/placeDE-overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/placeDE Template
 // @namespace    http://tampermonkey.net/
-// @version      4
+// @version      5
 // @description  try to take over the canvas!
 // @author       placeDE Devs
 // @match        https://garlic-bread.reddit.com/embed*
@@ -10,12 +10,15 @@
 // @downloadURL  https://github.com/PlaceDE-Official/place-overlay/raw/main/src/scripts/placeDE-overlay.user.js
 // ==/UserScript==
 
-let url = "https://place.army/overlay_target.png"
+let baseurl = "https://place.army/overlay_target.png"
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
         const canvasContainer = document.getElementsByTagName("garlic-bread-embed")[0].shadowRoot.children[0].getElementsByTagName("garlic-bread-canvas")[0].shadowRoot.children[0];
         const overlayImage = document.createElement("img");
-        overlayImage.src = url;
+        setInterval(() => {
+            overlayImage.src = `${baseurl}?${Math.random()}`;
+        }, 5000);
+        overlayImage.src = baseurl;
         overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 100opx;height: 1000px;`;
         canvasContainer.appendChild(overlayImage);
     }, false);
