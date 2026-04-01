77b09ceabe188c64de08774f331e325ad616e918
DevMiner <devminer@devminer.xyz>
2026-04-01 17:56:49 +0200

    r/tyles

--- src/scripts/placeDE-overlay.user.js
+++ src/scripts/placeDE-overlay.user.js
@@ -1,56 +1,51 @@
 // ==UserScript==
-// @name         r/placeDE Template
+// @name         [placeDE] r/tyles 2026
 // @namespace    http://tampermonkey.net/
-// @version      8.7
+// @version      9.0
 // @description  try to take over the canvas!
 // @author       placeDE Devs
-// @match        https://garlic-bread.reddit.com/embed*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @match        https://tyles.place/*
+// @match        https://tyles.place
+// @icon         https://www.redditstatic.com/desktop2x/img/favicon/favicon-32x32.png
 // @updateURL    https://github.com/PlaceDE-Official/place-overlay/raw/main/src/scripts/placeDE-overlay.user.js
 // @downloadURL  https://github.com/PlaceDE-Official/place-overlay/raw/main/src/scripts/placeDE-overlay.user.js
 // @run-at   document-start
 // ==/UserScript==
 
 const updateEvery = 30 * 1000;
-const src = "https://place.army/overlay_target.png";
-const style =
-  "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+const src = 'https://placede-official.github.io/pixel/overlay_target.png';
+const style = 'position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;';
 
 let overlayImage = null;
-if (window.top !== window.self) {
-  window.addEventListener(
-    "load",
-    () => {
-      const canvasContainer = document
-        .querySelector("garlic-bread-embed")
-        .shadowRoot.querySelector("garlic-bread-canvas")
-        .shadowRoot.querySelector(".container");
-      const canvas = canvasContainer.querySelector("canvas");
+addEventListener(
+	'load',
+	() => {
+		console.log('[PLACEDE] Overlay loading...');
 
-      overlayImage = document.createElement("img");
-      overlayImage.style = style;
+		const canvasContainer = document.querySelector('#canvas-container');
+		const canvas = canvasContainer.querySelector('canvas');
 
-      const updateImage = () => (overlayImage.src = src + "?" + Date.now());
+		overlayImage = document.createElement('img');
+		overlayImage.style = style;
 
-      updateImage();
-      setInterval(updateImage, updateEvery);
-      canvasContainer.appendChild(overlayImage);
+		const updateImage = () => (overlayImage.src = src + '?' + Date.now());
 
-      const canvasObserver = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-          if (mutation.type === "attributes") {
-            overlayImage.style.width =
-              mutation.target.getAttribute("width") + "px";
-            overlayImage.style.height =
-              mutation.target.getAttribute("height") + "px";
-          }
-        });
-      });
+		updateImage();
+		setInterval(updateImage, updateEvery);
+		canvasContainer.appendChild(overlayImage);
 
-      canvasObserver.observe(canvas, {
-        attributes: true,
-      });
-    },
-    false
-  );
-}
+		const canvasObserver = new MutationObserver((mutations) => {
+			mutations.forEach((mutation) => {
+				if (mutation.type === 'attributes') {
+					overlayImage.style.width = mutation.target.getAttribute('width') + 'px';
+					overlayImage.style.height = mutation.target.getAttribute('height') + 'px';
+				}
+			});
+		});
+
+		canvasObserver.observe(canvas, {
+			attributes: true
+		});
+	},
+	false
+);
